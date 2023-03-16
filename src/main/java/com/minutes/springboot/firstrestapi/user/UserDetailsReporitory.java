e073da36114759ed34285ae73ac73e735b6eeb16
FuerzaDON <luis_fernando_chavez@hotmail.com>
2023-03-16 01:35:32 -0600

    findByRole method added

--- src/main/java/com/minutes/springboot/firstrestapi/user/UserDetailsReporitory.java
+++
@@ -1,7 +0,0 @@
-package com.minutes.springboot.firstrestapi.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsReporitory extends JpaRepository<UserDetails, Long>{
-
-}
